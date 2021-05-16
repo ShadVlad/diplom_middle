2db293f5da8e3b5dd3497a3ecb953438789fa12e
Vladimir <wowchik.shadrin@yandex.ru>
2021-05-16 22:11:12 +0300

    gallery

---
+++ dist/main.94cc69fdb96087eca773.hot-update.js
@@ -0,0 +1,31 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatediplom_middle"]("main",{
+
+/***/ "./src/modules/sliderGallery.js":
+/*!**************************************!*\
+  !*** ./src/modules/sliderGallery.js ***!
+  \**************************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: D:\\\\Glo academy\\\\diplom_middle\\\\src\\\\modules\\\\sliderGallery.js: 'Const declarations' require an initialization value. (3:9)\\n\\n\\u001b[0m \\u001b[90m 1 |\\u001b[39m \\u001b[36mconst\\u001b[39m sliderGallery \\u001b[33m=\\u001b[39m () \\u001b[33m=>\\u001b[39m {\\u001b[0m\\n\\u001b[0m \\u001b[90m 2 |\\u001b[39m   \\u001b[36mconst\\u001b[39m slide \\u001b[33m=\\u001b[39m document\\u001b[33m.\\u001b[39mquerySelectorAll(\\u001b[32m\\\".gallery-slider\\\"\\u001b[39m)\\u001b[33m.\\u001b[39mchildNodes\\u001b[33m,\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 3 |\\u001b[39m   console\\u001b[33m.\\u001b[39mlog(\\u001b[32m'slide: '\\u001b[39m\\u001b[33m,\\u001b[39m slide)\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m   |\\u001b[39m          \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 4 |\\u001b[39m     slider \\u001b[33m=\\u001b[39m document\\u001b[33m.\\u001b[39mquerySelector(\\u001b[32m\\\".gallery-slider\\\"\\u001b[39m)\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 5 |\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 6 |\\u001b[39m   \\u001b[36mlet\\u001b[39m currentSlide \\u001b[33m=\\u001b[39m \\u001b[35m0\\u001b[39m\\u001b[33m,\\u001b[39m\\u001b[0m\\n    at Parser._raise (D:\\\\Glo academy\\\\diplom_middle\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:810:17)\\n    at Parser.raiseWithData (D:\\\\Glo academy\\\\diplom_middle\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:803:17)\\n    at Parser.raise (D:\\\\Glo academy\\\\diplom_middle\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:764:17)\\n    at Parser.parseVar (D:\\\\Glo academy\\\\diplom_middle\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13216:18)\\n    at Parser.parseVarStatement (D:\\\\Glo academy\\\\diplom_middle\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13026:10)\\n    at Parser.parseStatementContent (D:\\\\Glo academy\\\\diplom_middle\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12609:21)\\n    at Parser.parseStatement (D:\\\\Glo academy\\\\diplom_middle\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12542:17)\\n    at Parser.parseBlockOrModuleBlockBody (D:\\\\Glo academy\\\\diplom_middle\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13131:25)\\n    at Parser.parseBlockBody (D:\\\\Glo academy\\\\diplom_middle\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13122:10)\\n    at Parser.parseBlock (D:\\\\Glo academy\\\\diplom_middle\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13106:10)\");\n\n//# sourceURL=webpack://diplom_middle/./src/modules/sliderGallery.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("65680eb3b9a7f71b6df4")
+/******/ })();
+/******/ 
+/******/ }
+);
--- src/modules/sliderGallery.js
+++ src/modules/sliderGallery.js
@@ -1,3 +1,136 @@
-const sliderGallery = () => {};
+const sliderGallery = () => {
+  const //gallery = document.querySelector("#gallery"),
+    slider = document.querySelector(".gallery-slider"),
+    slide = slider.querySelectorAll(".slide"),
+    dots = slider.querySelector(".slider-dots");
+  //console.log("arrows: ", arrows);
+  //console.log("dots: ", dots);
+  let dot,
+    arrowsSpan = document.createElement("span"),
+    arrows = document.createElement("a");
+  arrows.classList.add("slider-arrow");
+  arrows.classList.add("prev");
+  // arrows.appendChild(arrowsSpan);
+  // slider.appendChild(arrows);
+  // arrowsSpan = document.createElement("span");
+  // arrows = document.createElement("a");
+  // arrows.classList.add("slider-arrow");
+  // arrows.classList.add("next");
+  // arrows.appendChild(arrowsSpan);
+  // slider.appendChild(arrows);
+  const addDot = () => {
+    console.log("dots: ", dots);
+    for (let i = 1; i <= slide.length; i++) {
+      let newDot = document.createElement("li"),
+        newBtn = document.createElement("button");
+      //console.log("newDot: ", newDot);
+      newDot.appendChild(newBtn);
+      //dots.appendChild(newDot);
+      //console.log("newDot: ", newDot);
+      newDot.classList.add("dot");
+      if (i === 1) {
+        newDot.classList.add("slick-active");
+      }
+      dots.appendChild(newDot);
+      //console.log("dots: ", dots);
+    }
+
+    dot = slider.querySelectorAll(".dot");
+    console.log("dot: ", dot);
+  };
+  let currentSlide = 0,
+    interval;
+
+  const prevSlide = (elem, index, strClass) => {
+    elem[index].classList.remove(strClass);
+    //console.log("elem[index]: ", elem[index].classList);
+  };
+
+  const nextSlide = (elem, index, strClass) => {
+    elem[index].classList.add(strClass);
+  };
+
+  const autoPlaySlide = () => {
+    //console.log("slide: ", slide);
+    //console.log("dot: ", dot);
+    //console.log("currentSlide: ", currentSlide);
+    prevSlide(slide, currentSlide, "slide-active");
+    prevSlide(dot, currentSlide, "slick-active");
+    currentSlide++;
+    currentSlide = currentSlide % slide.length;
+    //console.log("currentSlide: ", currentSlide);
+    nextSlide(slide, currentSlide, "slide-active");
+    nextSlide(dot, currentSlide, "slick-active");
+  };
+
+  const startSlide = (time = 3000) => {
+    interval = setInterval(autoPlaySlide, time);
+  };
+
+  const stopSlide = () => {
+    clearInterval(interval);
+  };
+
+  slider.addEventListener("click", (event) => {
+    console.log("event: ", event.target.matches("button"));
+    event.preventDefault();
+    let target = event.target;
+
+    if (!target.matches("button")) {
+      return;
+    }
+    prevSlide(slide, currentSlide, "slide-active");
+    //console.log("currentSlide: ", currentSlide);
+
+    prevSlide(dot, currentSlide, "slick-active");
+
+    if (target.matches("#arrow-right")) {
+      currentSlide++;
+    } else if (target.matches("#arrow-left")) {
+      currentSlide--;
+    } else if (target.closest("li")) {
+      //console.log('target.closest("li"): ', target.closest("li"));
+      dot.forEach((elem, index) => {
+        if (elem === target.closest("li")) {
+          currentSlide = index;
+          console.log("currentSlide: ", currentSlide);
+        }
+      });
+    }
+    //console.log("currentSlide: ", currentSlide);
+
+    if (currentSlide >= slide.length) {
+      currentSlide = 0;
+    }
+    if (currentSlide < 0) {
+      currentSlide = slide.length - 1;
+    }
+
+    nextSlide(slide, currentSlide, "slide-active");
+    nextSlide(dot, currentSlide, "slick-active");
+    //console.log("currentSlide: ", currentSlide);
+  });
+  slider.addEventListener("mouseover", (event) => {
+    //console.log("event: ", event.target.matches(".dot"));
+    if (
+      //event.target.matches(".portfolio-btn") ||
+      event.target.matches(".dot")
+    ) {
+      stopSlide();
+    }
+  });
+
+  slider.addEventListener("mouseout", (event) => {
+    if (
+      event.target.matches(".portfolio-btn") ||
+      event.target.matches(".dot")
+    ) {
+      startSlide();
+    }
+  });
+  addDot();
+  startSlide(1500);
+  //console.log("slider starts");
+};
 
 export default sliderGallery;
