f733b85c8688575662ca177dd47fb37ce8da0e57
Vladimir <wowchik.shadrin@yandex.ru>
2021-06-05 13:21:06 +0300

    main-slider

--- dist/dev-bundle.js
+++ dist/dev-bundle.js
@@ -71,7 +71,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nvar mainSlider = function mainSlider() {};\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (mainSlider);\n\n//# sourceURL=webpack://diplom_middle/./src/modules/mainSlider.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nvar mainSlider = function mainSlider() {\n  var //gallery = document.querySelector(\"#gallery\"),\n  slider = document.querySelector(\".main-slider\"),\n      slide = slider.querySelectorAll(\".slide\");\n  var currentSlide = 0,\n      interval;\n\n  var prevSlide = function prevSlide(elem, index, strClass) {\n    elem[index].classList.remove(strClass); //console.log(\"elem[index]: \", elem[index].classList);\n  };\n\n  var nextSlide = function nextSlide(elem, index, strClass) {\n    elem[index].classList.add(strClass);\n    console.log(\"elem[index]: \", elem[index].classList);\n  };\n\n  var autoPlaySlide = function autoPlaySlide() {\n    //console.log(\"slide: \", slide);\n    //console.log(\"dot: \", dot);\n    //console.log(\"currentSlide: \", currentSlide);\n    prevSlide(slide, currentSlide, \"slide-active\");\n    currentSlide++;\n    currentSlide = currentSlide % slide.length;\n    console.log(\"currentSlide: \", currentSlide);\n    nextSlide(slide, currentSlide, \"slide-active\");\n  };\n\n  var startSlide = function startSlide() {\n    var time = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 5000;\n    interval = setInterval(autoPlaySlide, time);\n  };\n\n  var stopSlide = function stopSlide() {\n    clearInterval(interval);\n  };\n\n  startSlide(1500);\n};\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (mainSlider);\n\n//# sourceURL=webpack://diplom_middle/./src/modules/mainSlider.js?");
 
 /***/ }),
 
@@ -539,7 +539,7 @@ eval("var map = {\n\t\"./log\": \"./node_modules/webpack/hot/log.js\"\n};\n\n\nf
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("f543ef5b281365ed190d")
+/******/ 		__webpack_require__.h = () => ("d0420716467eb7fc83fa")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/modules/mainSlider.js
+++ src/modules/mainSlider.js
@@ -1,3 +1,39 @@
-const mainSlider = () => {};
+const mainSlider = () => {
+  const //gallery = document.querySelector("#gallery"),
+    slider = document.querySelector(".main-slider"),
+    slide = slider.querySelectorAll(".slide");
+
+  let currentSlide = 0,
+    interval;
+  const prevSlide = (elem, index, strClass) => {
+    elem[index].classList.remove(strClass);
+    //console.log("elem[index]: ", elem[index].classList);
+  };
+
+  const nextSlide = (elem, index, strClass) => {
+    elem[index].classList.add(strClass);
+    console.log("elem[index]: ", elem[index].classList);
+  };
+  const autoPlaySlide = () => {
+    //console.log("slide: ", slide);
+    //console.log("dot: ", dot);
+    //console.log("currentSlide: ", currentSlide);
+    prevSlide(slide, currentSlide, "slide-active");
+
+    currentSlide++;
+    currentSlide = currentSlide % slide.length;
+    console.log("currentSlide: ", currentSlide);
+    nextSlide(slide, currentSlide, "slide-active");
+  };
+
+  const startSlide = (time = 5000) => {
+    interval = setInterval(autoPlaySlide, time);
+  };
+
+  const stopSlide = () => {
+    clearInterval(interval);
+  };
+  startSlide(1500);
+};
 
 export default mainSlider;
